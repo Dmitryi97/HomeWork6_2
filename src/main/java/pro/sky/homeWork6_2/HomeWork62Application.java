7592b96278b65ca700c9485f95403739b34080f1
Dmitryi <pytin97@bk.ru>
2024-05-01 14:20:17 +0300

    Initial commit!

---
+++ src/main/java/pro/sky/homeWork6_2/HomeWork62Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWork6_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork62Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork62Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/homeWork6_2/HomeWork62ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homeWork6_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork62ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
